10d1ac45a429c7f9989e7d480e311e18e97a5ea6
Michelle Kim <pasolre95kim@gmail.com>
2018-06-08 18:51:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return"Happy holidays, you!"
+  return`Happy holidays, ${name}!`
 }
 
 function happyHolidaysTo(holiday){
@@ -14,5 +14,5 @@ function happyHolidayTo(holiday) {
   return "Happy Independence Day, you!"
 }
 function holidayCountdown(holiday,days){
-  return "It's 20 days until Mother's Day!"
+  return `It's ${days} days until ${holiday}!`
 }
